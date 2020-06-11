f13e80442083394089c5a50fbf2af28e71ba65ee
Andrew Hart <ahart@cern.ch>
2020-06-11 09:39:39 -0400

    Removed debug stuff.

--- TrackletAlgorithm/TrackBuilder.cc
+++ TrackletAlgorithm/TrackBuilder.cc
@@ -35,7 +35,6 @@ void TrackBuilder(
   tracks.clear(bx);
   unsigned short nTracks = 0;
 
-  std::cout << "[" << __FILE__ << ":" << __LINE__ << "] " << __PRETTY_FUNCTION__ << std::endl; // HART
   tracks : for (unsigned short i = 0; i < kMaxProc; i++) {
 #pragma HLS pipeline II=1
 
@@ -47,8 +46,6 @@ void TrackBuilder(
     const short &TCID = iTPARMem;
     const TrackletParameters &tpar = trackletParameters[iTPARMem].read_mem(bx, iTPAR);
     TrackFit tf(1, TCID >> 4, tpar.getRinv(), tpar.getPhi0(), tpar.getZ0(), tpar.getT());
-    std::cout << "  tracklet " << std::dec << i << std::hex << ": seed type = " << (TCID >> 4) << ", rinv = " << tpar.getRinv() << ", phi0 = " << tpar.getPhi0() << ", z0 = " << tpar.getZ0() << ", t = " << tpar.getT() << std::dec << std::endl; // HART
-    std::cout << "    track fit object: seed type = " << std::hex << tf.getSeedType() << ", rinv = " << tf.getRinv() << ", phi0 = " << tf.getPhi0() << ", z0 = " << tf.getZ0() << ", t = " << tf.getT() << std::dec << std::endl; // HART
 
     if (!done) {
       barrel_full_match_memories : for (unsigned short j = 0; j < 16; j++) {
@@ -99,9 +96,6 @@ void TrackBuilder(
       }
     }
     if (tf.getNMatches() >= 2)
-    { // HART
-      std::cout << "  SAVING TRACK..." << std::endl; // HART
       tracks.write_mem(bx, tf, nTracks++);
-    } // HART
   }
 }
