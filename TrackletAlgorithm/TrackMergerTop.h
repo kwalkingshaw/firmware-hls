f8ca1cac11889306ffc113028eb95ea71307fe4f
Katie Walkingshaw Pass <katie.walkingshawpass@bristol.ac.uk>
2021-01-20 09:18:44 +0000

    Adding top header file

---
+++ TrackletAlgorithm/TrackMergerTop.h
@@ -0,0 +1,10 @@
+#ifndef TrackletAlgorithm TrackMergerTop_h
+#define TrackletAlgorithm TrackMergerTop_h
+
+#include "TrackMerger.h"
+
+void TrackMergerTop(const BXType bx,
+                    TrackFitMemory inputTracks,
+                    TrackFitMemory outputTracks);
+
+#endif
