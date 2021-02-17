a68fdd122f5195a8c8a6e22c33402cacb62f9e0a
Katie Walkingshaw Pass <katie.walkingshawpass@bristol.ac.uk>
2021-02-17 16:57:19 +0000

    Dummy module work

--- TrackletAlgorithm/TrackMerger.cc
+++ TrackletAlgorithm/TrackMerger.cc
@@ -5,4 +5,16 @@ void TrackMerger(const BXType bx, TrackFitMemory inputTracks, TrackFitMemory out
         auto inputTrack = inputTracks.read_mem(bx, read_addr);
          outputTracks.write_mem(bx, inputTrack, read_addr);
     }
+
+
+
+    // Comparison module
+    //count stubs in common, looking at stubs with smallest phi residual
+
+
+    // if there are > 3 stubs in common, merge tracks into single track
+
+    // assign helix params according to best seed type
+
+
 }
---
+++ TrackletAlgorithm/TrackMergerTop.cc
@@ -0,0 +1,7 @@
+#include "TrackMergerTop.h"
+void TrackMerger(const BXType bx, TrackFitMemory inputTracks, TrackFitMemory outputTracks){
+    for(int i =0; i < inputTracks.getEntries(bx); i++){
+        auto inputTrack = inputTracks.read_mem(bx, i, read_addr)
+         outputTracks.write_mem(bx, inputTrack, read_addr);
+    }
+}
